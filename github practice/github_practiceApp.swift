a9f505d4f9ff687d5e25e6f341eb1d66a08459cc
Nicole Stekol <npstekol@gmail.com>
2023-07-27 13:13:10 -0400

    Initial Commit

---
+++ github practice/github_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_practiceApp.swift
+//  github practice
+//
+//  Created by Scholar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct github_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
